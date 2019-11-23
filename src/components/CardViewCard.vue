fe78cbc69f4ca638bda127323b84a72ec41464b1
Andone Alexandru <andonealexandru19@yahoo.com>
2019-11-23 13:55:44 +0200

    Added navbar and card view

--- src/components/CardViewCard.vue
+++ src/components/CardViewCard.vue
@@ -1,30 +1,27 @@
 <template>
-  <div>
-    <b-card
-      :title="name"
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-    >
-    <b-card-text>
-      Price: {{ product.unitPrice }} RON / {{ product.unit }}
-      Description: {{ product.description }}
-    </b-card-text>
+  <b-card
+    :title="name"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2"
+  >
+  <b-card-text>
+    Price: {{ product.unitPrice }} RON / {{ product.unit }}
+    Description: {{ product.description }}
+  </b-card-text>
 
-    <b-button href="#" variant="primary">See more</b-button>
-  </b-card>
-  </div>
+  <b-button href="#" variant="primary">See more</b-button>
+</b-card>
 </template>
 
 <script>
 /* eslint-disable */
 export default {
-  data () {
-    return {
-      product: {
+  props: {
+    product: {
         name: '',
         description: '',
         userId: '',
@@ -32,7 +29,10 @@ export default {
         unitPrice: '',
         availableUnits: '',
         subcategoryId: ''
-      },
+      }
+  },
+  data () {
+    return {
       subcategory: {
         category: '',
         name: ''
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,125 @@
 <template>
   <div id="home">
+  <b-card-group>
+    <div v-for="product in products" :key="product" class="cards">
+    <CardView
+      :product="product"
+    />
+    </div>
+  </b-card-group>
   </div>
 </template>
 
 <script>
-
 /* eslint-disable */
+import CardView from '../components/CardViewCard'
 
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    CardView
+  },
+  data() {
+    return {
+      products: [
+        {
+          name: 'lkknsv',
+          description: 'ldsvn',
+          userId: 'vobdsjx ',
+          unit: 'buc',
+          unitPrice: '5',
+          availableUnits: '100',
+          subcategoryId: '5'
+        },
+        {
+          name: 'bjwkvn',
+          description: 'bewj',
+          userId: 'webjk',
+          unit: 'kg',
+          unitPrice: '10',
+          availableUnits: '200',
+          subcategoryId: '5'
+        },
+        {
+          name: 'lkknsv',
+          description: 'ldsvn',
+          userId: 'vobdsjx ',
+          unit: 'buc',
+          unitPrice: '5',
+          availableUnits: '100',
+          subcategoryId: '5'
+        },
+        {
+          name: 'bjwkvn',
+          description: 'bewj',
+          userId: 'webjk',
+          unit: 'kg',
+          unitPrice: '10',
+          availableUnits: '200',
+          subcategoryId: '5'
+        },
+        {
+          name: 'lkknsv',
+          description: 'ldsvn',
+          userId: 'vobdsjx ',
+          unit: 'buc',
+          unitPrice: '5',
+          availableUnits: '100',
+          subcategoryId: '5'
+        },
+        {
+          name: 'bjwkvn',
+          description: 'bewj',
+          userId: 'webjk',
+          unit: 'kg',
+          unitPrice: '10',
+          availableUnits: '200',
+          subcategoryId: '5'
+        },
+        {
+          name: 'lkknsv',
+          description: 'ldsvn',
+          userId: 'vobdsjx ',
+          unit: 'buc',
+          unitPrice: '5',
+          availableUnits: '100',
+          subcategoryId: '5'
+        },
+        {
+          name: 'bjwkvn',
+          description: 'bewj',
+          userId: 'webjk',
+          unit: 'kg',
+          unitPrice: '10',
+          availableUnits: '200',
+          subcategoryId: '5'
+        },
+        {
+          name: 'lkknsv',
+          description: 'ldsvn',
+          userId: 'vobdsjx ',
+          unit: 'buc',
+          unitPrice: '5',
+          availableUnits: '100',
+          subcategoryId: '5'
+        },
+        {
+          name: 'bjwkvn',
+          description: 'bewj',
+          userId: 'webjk',
+          unit: 'kg',
+          unitPrice: '10',
+          availableUnits: '200',
+          subcategoryId: '5'
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style>
+.cards {
+  margin: 10px;
+}
+</style>
